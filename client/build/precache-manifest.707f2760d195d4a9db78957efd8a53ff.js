01d60511259976b81880fa5d44f8156cdba746f7
cgad <35946778+cgad@users.noreply.github.com>
2018-10-15 14:37:47 -0600

    Take build out of .gitignore

---
+++ client/build/precache-manifest.707f2760d195d4a9db78957efd8a53ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a70358ecf7493ae17a65",
+    "url": "/static/js/main.a70358ec.chunk.js"
+  },
+  {
+    "revision": "550d6fa4a3864f2dfb65",
+    "url": "/static/js/1.550d6fa4.chunk.js"
+  },
+  {
+    "revision": "a70358ecf7493ae17a65",
+    "url": "/static/css/main.b48de18a.chunk.css"
+  },
+  {
+    "revision": "6eecfb6bd6df7e9a86d9aea5199bd6e6",
+    "url": "/index.html"
+  }
+];
