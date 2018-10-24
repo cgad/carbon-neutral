16ce6d39b7ce60975a3b24c35df42b2ebc9ce01c
cgad <35946778+cgad@users.noreply.github.com>
2018-10-23 20:34:40 -0600

    Fix flight page routing path bug, readme typo

--- client/src/utils/APIflight.js
+++ client/src/utils/APIflight.js
@@ -3,17 +3,17 @@ import axios from "axios";
 export default {
     // add getCurrentSearch
     getAllFlights: function() {
-        return axios.get("/api/flight/calculate")
+        return axios.get("/api/flight")
     },
     getFlight: function(id) {
-        return axios.get("/api/flight/calculate" + id);
+        return axios.get("/api/flight/" + id);
     },
     deleteFlight: function(id) {
-        return axios.delete("api/flight/calculate/" + id);
+        return axios.delete("api/flight/" + id);
     },
     // to "favorite" a search
     saveFlight: function(flightData) {
-        return axios.post("/api/flight/calculate/", flightData)
+        return axios.post("/api/flight/", flightData)
     },
     viewFlight: function(id) {
         return axios.get("/api/flight/view/" + id)
