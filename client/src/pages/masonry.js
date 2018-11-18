12d3d149059f46993c6a13ae83abdd8fbc7625ea
cgad <35946778+cgad@users.noreply.github.com>
2018-11-17 18:41:39 -0700

    Add image overlay fade

--- client/src/pages/masonry.js
+++ client/src/pages/masonry.js
@@ -12,20 +12,27 @@ class Masonry extends Component {
     return (
       <div>
         <Nav></Nav>
-        <div class="masonry">
-          {/* <div class="item">
+        <div className="masonry sibling-fade">
+          <div className="masonry-brick">
             blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
-          </div> */}
-          <div class="masonry-brick">
+          </div>
+          <div className="masonry-brick container">
+          {/* change this to opposite- show text, then show image on hover */}
+            <img src="images/runway.jpg" className="runway" alt="eye"></img>
+            <div className="overlay">
+              <div className="text">blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah</div>
+            </div>
+          </div>
+          <div className="masonry-brick about">
             <Link to="/">About</Link>
           </div>
-          <div class="masonry-brick">
+          <div className="masonry-brick">
             <Link to="/flight">Flight</Link>
           </div>
-          <div class="masonry-brick">
+          <div className="masonry-brick">
             <Link to="/roadtrip">Road Trip</Link>
           </div>
-          <div class="masonry-brick">
+          <div className="masonry-brick">
             <Link to="/pet">Pet</Link>
           </div>
         </div>
